2cc82bfcd7509fe232d8472e538df445afe1a83c
Mohammad Nafis Fuad <166198863+nafis2024@users.noreply.github.com>
2025-07-31 07:40:59 +0000

    Crap

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    char problem[] = "I don't like how messy the terminal looks. Worse than local build and codeblocks.";
+    printf("Hello world!!\n");
+    printf("This is kinda my first line of code");
+    printf("%s", problem);
+}
